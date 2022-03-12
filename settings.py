a6124a625c7e0613003d867ea99d678a0620f057
NKDuy <kn145660@gmail.com>
2022-03-12 20:30:22 +0700

    Setting up the level

---
+++ settings.py
@@ -0,0 +1,16 @@
+level_map = [
+'                            ',
+'                            ',
+'                            ',
+' XX    XXX            XX    ',
+' XX                         ',
+' XXXX         XX         XX ',
+' XXXX       XX              ',
+' XX    X  XXXX    XX  XX    ',
+'       X  XXXX    XX  XXX   ',
+'    XXXX  XXXXXX  XX  XXXX  ',
+'XXXXXXXX  XXXXXX  XX  XXXX  ']
+
+tile_size = 56
+screen_width = 1080
+screen_height = len(level_map) * tile_size
