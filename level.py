080371917464cd07ed6ac9f4362e19216d14ec19
NKDuy <kn145660@gmail.com>
2022-03-14 14:44:30 +0700

    Creating the player 'camera'

--- level.py
+++ level.py
@@ -1,6 +1,7 @@
+from platform import platform
 import pygame
 from tiles import Tile
-from settings import tile_size
+from settings import tile_size, screen_width
 from player import Player
 
 class Level:
@@ -28,6 +29,21 @@ class Level:
                     player_sprite = Player((x,y))
                     self.player.add(player_sprite)
 
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if player_x < screen_width / 4 and direction_x < 0:
+            self.world_shift = 8
+            player.speed = 0
+        elif player_x > screen_width - (screen_width / 4) and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
+    
     def run(self):
 
         # level tiles
@@ -37,3 +53,4 @@ class Level:
         # player
         self.player.update()
         self.player.draw(self.display_surface)
+        self.scroll_x()
